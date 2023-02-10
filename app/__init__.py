241ea2b78aec80ed9fad9520107ff67efa2ecf3d
onedrako <onedrako@gmail.com>
2023-02-10 11:37:38 -0600

    prueba

--- app/__init__.py
+++ app/__init__.py
@@ -2,18 +2,19 @@ import os
 
 from flask import Flask
 
+
 def create_app():
     app = Flask(__name__)
 
     app.config.from_mapping(
-        FROM_EMAIL=os.environ.get('FROM_EMAIL'),
-        SENDGRID_KEY=os.environ.get('SENDGRID_API_KEY'),
-        SECRET_KEY=os.environ.get('SECRET_KEY'),
+        DATABASE=os.environ.get('FLASK_DATABASE'),
         DATABASE_HOST=os.environ.get('FLASK_DATABASE_HOST'),
         DATABASE_PASSWORD=os.environ.get('FLASK_DATABASE_PASSWORD'),
         DATABASE_USER=os.environ.get('FLASK_DATABASE_USER'),
         DATABASE_PORT=os.environ.get('FLASK_DATABASE_PORT'),
-        DATABASE=os.environ.get('FLASK_DATABASE'),
+        SECRET_KEY=os.environ.get('SECRET_KEY'),
+        SENDGRID_KEY=os.environ.get('SENDGRID_API_KEY'),
+        FROM_EMAIL=os.environ.get('FROM_EMAIL'),
     )
 
     from . import db
